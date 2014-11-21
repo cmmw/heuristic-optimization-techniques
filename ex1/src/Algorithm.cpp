b6aa32e4431c4b05a1aa3c62e010eae3735a8f21
Christian Wagner <christian.wagner86@gmx.at>
2014-11-21 17:55:36 +0100

    Added main

---
+++ ex1/src/Algorithm.cpp
@@ -0,0 +1,23 @@
+/*
+ * Algorithm.cpp
+ *
+ *  Created on: Nov 21, 2014
+ *      Author: christian
+ */
+
+#include "Algorithm.h"
+
+namespace tcbvrp
+{
+
+Algorithm::Algorithm()
+{
+	// TODO Auto-generated constructor stub
+}
+
+Algorithm::~Algorithm()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace tcbvrp */
---
+++ ex1/src/Algorithm.h
@@ -0,0 +1,23 @@
+/*
+ * Algorithm.h
+ *
+ *  Created on: Nov 21, 2014
+ *      Author: christian
+ */
+
+#ifndef ALGORITHM_H_
+#define ALGORITHM_H_
+
+namespace tcbvrp
+{
+
+class Algorithm
+{
+public:
+	Algorithm();
+	virtual ~Algorithm();
+};
+
+} /* namespace tcbvrp */
+
+#endif /* ALGORITHM_H_ */
