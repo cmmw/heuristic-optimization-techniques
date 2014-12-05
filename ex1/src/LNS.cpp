1a810736b1485ffac60411947178b4d911518a6c
Felix Winter <ixos01@gmail.com>
2014-12-05 19:39:13 +0100

    Small improvement in LNS, only the lastTour is checked to determine feasibility of initSolution

--- ex1/src/LNS.cpp
+++ ex1/src/LNS.cpp
@@ -36,14 +36,15 @@ void LNS::solve()
 
 	// check if initial solution is feasible and set bestCosts if so
 	bool feasible = true;
-	for (std::vector<std::vector<Node*> >::iterator tour = solution->getTours().begin(); tour != solution->getTours().end(); tour++)
-	{
-		if (Algorithm::calcTourCosts(*tour, graph.getAdjacencyMatrix()) > graph.getGlobalTimeLimit())
+	// only the last one has to be checked, the others will be correct anyway right?
+	//for (std::vector<std::vector<Node*> >::iterator tour = solution->getTours().begin(); tour != solution->getTours().end(); tour++)
+	//{
+		if (Algorithm::calcTourCosts(solution->getTours().end()*, graph.getAdjacencyMatrix()) > graph.getGlobalTimeLimit())
 		{
 			feasible = false;
 			break;
 		}
-	}
+	//}
 
 	if (feasible)
 		bestCosts = solution->getTotalCosts();
