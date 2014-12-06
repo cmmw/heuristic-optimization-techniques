a85363930940f5e220de1b73c780738ea099c269
Felix Winter <ixos01@gmail.com>
2014-12-06 01:26:02 +0100

    Some fixes in irace config

--- ex1/tuning/makeInstanceList.py
+++ ex1/tuning/makeInstanceList.py
@@ -5,5 +5,8 @@ f = open('instances-list', 'w')
 numberOfTimes = 30
 
 for x in range(0, numberOfTimes):
-            instanceFile = "./Instances/tcbvrp_700.prob\n"
+            instanceFile = "tcbvrp_700.prob\n"
             f.write(instanceFile)  
+
+
+
